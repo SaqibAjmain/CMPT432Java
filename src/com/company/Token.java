d2ad1fde7eb2b02c9a0f07c9f4b192e3c685df1d
SaqibAjmain <46943721+SaqibAjmain@users.noreply.github.com>
2022-03-14 19:16:17 -0400

    Lexer Class

--- src/com/company/Token.java
+++ src/com/company/Token.java
@@ -13,4 +13,26 @@ public class Token {
         this.line = line;
         this.charAt = charAt;
     }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getVal() {
+        return val;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public int getCharAt(){
+        return charAt;
+    }
+
+    public String LexMessage(){
+        return ("DEBUG Lexer - " + getType() + "[ " + getVal() + " ]" + " found at (" + getLine() + ":" + getCharAt() + ")")
+    }
+
+    
 }
