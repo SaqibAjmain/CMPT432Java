d4aee53d749a91eb39ba4194f93dc8682a1f3ab8
SaqibAjmain <46943721+SaqibAjmain@users.noreply.github.com>
2022-03-14 18:32:24 -0400

    token constructor

--- src/com/company/Token.java
+++ src/com/company/Token.java
@@ -1,4 +1,5 @@
 package com.company;
+import java.util.regex.*;
 
 public class Token {
     private String type;
@@ -6,5 +7,10 @@ public class Token {
     private int line;
     private int charAt;
 
-
+    public Token(String type, String val, int line, int charAt){
+        this.type = type;
+        this.val = val;
+        this.line = line;
+        this.charAt = charAt;
+    }
 }
